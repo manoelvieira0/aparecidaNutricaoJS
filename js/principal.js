1985dcd1055170ac4e03e6c231db62697e74ead6
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-05 14:23:44 -0300

    Buscando dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,11 @@
-
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'Aparecida Nutricionista'
+
+var paciente = document.querySelector("#primeiro-paciente")
+var tdPeso = paciente.querySelector(".info-peso")
+var tdAltura = paciente.querySelector(".info-altura")
+var peso = tdPeso.textContent
+var altura = tdAltura.textContent
+
+var imc = peso / altura*altura
+console.log(imc)

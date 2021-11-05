62a26895e8c79ad96289ebde6252f646ce4bb705
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-05 15:16:39 -0300

    Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -7,5 +7,21 @@ var tdAltura = paciente.querySelector(".info-altura")
 var peso = tdPeso.textContent
 var altura = tdAltura.textContent
 var tdImc = paciente.querySelector(".info-imc")
-var imc = peso / (altura*altura)
-tdImc.textContent = imc
+
+var pesoEhValido = true
+var alturaEhValida = true
+if (peso < 0 || peso > 1000) {
+    console.log('Peso inválido')
+    pesoEhValido = false
+    tdImc.textContent = 'Peso Inválido!'
+}
+if (altura < 0 || altura > 3) {
+    console.log('Altura Inválida')
+    alturaEhValida = false
+    tdImc.textContent = 'Altura Inválido!'
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc
+}

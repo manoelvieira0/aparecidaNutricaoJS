3c58a65d8f37f36fea8f426c3071a65a1d30306e
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-18 16:09:23 -0300

    Capturando erros de requisição

--- js/buscarpaciente.js
+++ js/buscarpaciente.js
@@ -8,11 +8,21 @@ botaoAdicionar.addEventListener("click", function () {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes")
 
     xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText
-        var pacientes = JSON.parse(resposta)
-        pacientes.forEach(function (paciente) {
-            adicionaPacienteNaTabela(paciente)
-        })
+        var erroajax = document.querySelector("#erro-ajax")
+        if(xhr.status == 200){
+            erroajax.classList.add("invisivel")
+            var resposta = xhr.responseText
+            var pacientes = JSON.parse(resposta)
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente)
+            })
+        }else{
+            console.log(xhr.status)
+            console.log(xhr.responseText)
+            erroajax.classList.remove("invisivel")
+
+        }
+        
     })
 
     xhr.send()

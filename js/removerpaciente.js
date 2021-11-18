cf4b9daa5ed248f461966c03a472b9e3c2372f1e
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-18 08:52:52 -0300

    Delegando eventos

--- js/removerpaciente.js
+++ js/removerpaciente.js
@@ -1,8 +1,15 @@
 var pacientes = document.querySelectorAll(".paciente")
 
+var tabela = document.querySelector('table')
+tabela.addEventListener('dblclick', function (event) {
+    event.target.parentNode.remove()
+
+})
+/*
 pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick', function(){
         console.log('Fui Clicado')
         this.remove()
     })
 })
+*/
